0106bdba7e2262636683f2c5cc83842dbc2365e7
waleed2000x <waleedahmed2000x@gmail.com>
2024-05-26 23:07:17 +0500

    Frontend | Login Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
+import Login from "./pages/login/Login";
+import "../scss/index.css";
 export default function App() {
   return (
     <main>
-      <div className="home"></div>
+      <div className="app">
+        <Login />
+      </div>
     </main>
   );
 }
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,9 +1,102 @@
 import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "../../components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+
+const FormSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: "Username must be at least 2 characters." })
+    .trim() // Ensure no leading/trailing spaces
+    .regex(/^[^ ]*?[^ ]+$/, "Username cannot contain spaces."),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long." })
+    .regex(/[0-9]/, "Password must contain at least one number."),
+});
 
 export default function Login() {
+  const form = useForm({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
   return (
-    <div>
+    <div className="login-parent">
       <p>Login</p>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-2/3 space-y-6"
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name. Must be at least 2
+                  characters and cannot contain spaces.
+                </FormDescription>
+                {form.errors && form.errors.username && (
+                  <FormMessage>{form.errors.username.message}</FormMessage>
+                )}
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="••••••••" {...field} />
+                </FormControl>
+                <FormDescription>
+                  Must be at least 8 characters long and contain at least one
+                  number.
+                </FormDescription>
+                {form.errors && form.errors.password && (
+                  <FormMessage>{form.errors.password.message}</FormMessage>
+                )}
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
   );
 }
